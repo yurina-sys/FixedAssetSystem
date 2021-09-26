7140ebe6aeb4247e807b28ade16ab414dd0a61ba
齋藤百合奈 <pee199605ys@gmail.com>
2021-09-27 00:44:19 +0900

    tsvファイルからデータ取得、とりあえず出力まで

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
-test.
-test2.
+<html>
+<head>
+    <title>墨田区固定資産台帳表示＆検索</title>
+</head>
+
+<body>
+
+<?php
+$fileData = new SplFileObject('墨田区固定資産台帳_202103.tsv');
+$fileData -> setFlags(SplFileObject::READ_CSV);
+$fileData -> setCsvControl("t");
+
+foreach($fileData as $data) {
+
+    $str = implode($data);
+    $str = mb_convert_encoding($str, "UTF-8", "SJIS");
+    echo($str);
+    // var_dump(implode($data));
+}
+?>
+
+
+
+
+</body>
+
+</html>
