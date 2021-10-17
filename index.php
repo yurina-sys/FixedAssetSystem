afb420dbaf153e5fee69491d4e7c8e5b3612f33e
齋藤百合奈 <pee199605ys@gmail.com>
2021-10-17 18:42:33 +0900

    全体的なリファクタ・ページネーション大枠実装

--- index.php
+++ index.php
@@ -7,87 +7,107 @@
 <body>
 
 <?php
-$property_number_text = isset($_GET["財産番号"]) ? $_GET["財産番号"] : "";
-$asset_name_text = isset($_GET["資産名称"]) ? $_GET["資産名称"] : "";
-$location_text = isset($_GET["所在地"]) ? $_GET["所在地"] : "";
-$auxiliary_submit_name_text = isset($_GET["補助科目名称"]) ? $_GET["補助科目名称"] : "";
-$acquisition_price_min_text = isset($_GET["最小取得価格"]) ? $_GET["最小取得価格"] : "";
-$acquisition_price_max_text = isset($_GET["最大取得価格"]) ? $_GET["最大取得価格"] : "";
-$book_value_of_period_min_text = isset($_GET["最小期末簿価"]) ? $_GET["最小期末簿価"] : "";
-$book_value_of_period_max_text = isset($_GET["最大期末簿価"]) ? $_GET["最大期末簿価"] : "";
+$input_property_number = isset($_GET["property_number"]) ? $_GET["property_number"] : "";
+$input_asset_name = isset($_GET["asset_name"]) ? $_GET["asset_name"] : "";
+$input_location = isset($_GET["location"]) ? $_GET["location"] : "";
+$input_auxiliary_submit_name = isset($_GET["auxiliary_submit_name"]) ? $_GET["auxiliary_submit_name"] : "";
+$input_acquisition_price_min = isset($_GET["acquisition_price_min"]) ? $_GET["acquisition_price_min"] : "";
+$input_acquisition_price_max = isset($_GET["acquisition_price_max"]) ? $_GET["acquisition_price_max"] : "";
+$input_book_value_of_period_min = isset($_GET["book_value_of_period_min"]) ? $_GET["book_value_of_period_min"] : "";
+$input_book_value_of_period_max = isset($_GET["book_value_of_period_max"]) ? $_GET["book_value_of_period_max"] : "";
 ?>
 
 <form action="index.php" method="get">
   <p>
      財産番号
-     <input type="text" name="財産番号" value="<?php echo $property_number_text; ?>">
+     <input type="text" name="property_number" value="<?php echo $input_property_number; ?>">
      資産名称
-     <input type="text" name="資産名称" value="<?php echo $asset_name_text; ?>">
+     <input type="text" name="asset_name" value="<?php echo $input_asset_name; ?>">
      所在地
-     <input type="text" name="所在地" value="<?php echo $location_text; ?>">
+     <input type="text" name="location" value="<?php echo $input_location; ?>">
      補助科目名称
-     <input type="text" name="補助科目名称" value="<?php echo $auxiliary_submit_name_text; ?>">
+     <input type="text" name="auxiliary_submit_name" value="<?php echo $input_auxiliary_submit_name; ?>">
      取得価格
-     <input type="text" name="最小取得価格" value="<?php echo $acquisition_price_min_text; ?>">
+     <input type="text" name="acquisition_price_min" value="<?php echo $input_acquisition_price_min; ?>">
      〜
-     <input type="text" name="最大取得価格" value="<?php echo $acquisition_price_max_text; ?>">
+     <input type="text" name="acquisition_price_max" value="<?php echo $input_acquisition_price_max; ?>">
      期末簿価
-     <input type="text" name="最小期末簿価" value="<?php echo $book_value_of_period_min_text; ?>">
+     <input type="text" name="book_value_of_period_min" value="<?php echo $input_book_value_of_period_min; ?>">
      〜
-     <input type="text" name="最大期末簿価" value="<?php echo $book_value_of_period_max_text; ?>">
+     <input type="text" name="book_value_of_period_max" value="<?php echo $input_book_value_of_period_max; ?>">
      <br>
      <input type="submit" value="submit">
   </p>
  </form>
 
- <a href="index.php?page=1">1</a>
-
-
 <?php
 // 文字列検索
-const PROPERTY_NUMBER = "財産番号";
-const ASSET_NAME = "資産名称";
-const LOCATION = "所在地";
-const AUXILIARY_SUBJECT_NAME = "補助科目名称";
+const PROPERTY_NUMBER = 0;
+const ASSET_NAME = 2;
+const LOCATION = 3;
+const AUXILIARY_SUBJECT_NAME = 5;
 
 // 範囲検索
 const ACQUISITION_PRICE_MIN = "最小取得価格";
 const ACQUISITION_PRICE_MAX = "最大取得価格";
 const BOOK_VALUE_OF_PERIOD_MIN = "最小期末簿価";
 const BOOK_VALUE_OF_PERIOD_MAX = "最大期末簿価";
-// const SERCH_TERMS_ARRAY = ["財産番号", "資産名称", "所在地", "補助科目名称", "最小取得価格", "最大取得価格", "最小期末簿価", "最大期末簿価"];
-const SERCH_TERMS_ARRAY = ["財産番号", "資産名称", "所在地", "補助科目名称"];
 
 const PAGE_ITEM_COUNT = 20;
 
-$current_page = isset($_GET["page"]);
-print_r($current_page);
+// 絞り込み処理実行
+[$header_array,$result_array] = searchDatas($input_property_number,
+                                                $input_asset_name,
+                                                $input_location,
+                                                $input_auxiliary_submit_name,
+                                                $input_acquisition_price_min,
+                                                $input_acquisition_price_max,
+                                                $input_book_value_of_period_min,
+                                                $input_book_value_of_period_max);
 
-if(!isset($_GET["page"])) {
-    $input_array = Array();
-    $input_array[PROPERTY_NUMBER] = $_GET[PROPERTY_NUMBER];
-    $input_array[ASSET_NAME] = $_GET[ASSET_NAME];
-    $input_array[LOCATION] = $_GET[LOCATION];
-    $input_array[AUXILIARY_SUBJECT_NAME] = $_GET[AUXILIARY_SUBJECT_NAME];
-// $input_array[ACQUISITION_PRICE_MIN] = $_GET[ACQUISITION_PRICE_MIN];
-// $input_array[ACQUISITION_PRICE_MAX] = $_GET[ACQUISITION_PRICE_MAX];
-// $input_array[BOOK_VALUE_OF_PERIOD_MIN] = $_GET[BOOK_VALUE_OF_PERIOD_MIN];
-// $input_array[BOOK_VALUE_OF_PERIOD_MAX] = $_GET[BOOK_VALUE_OF_PERIOD_MAX];
-// 配列化した検索条件の未入力を除いた配列を作りたい（予定）
-    foreach(array_values($input_array) as $array) {
-        if($array != "") {
-            // 検索条件があったら絞り込み処理実行
-            searchDatas($input_array);
-        } else {
-            // 入力値なかったら全件表示
-        }
-    }
+$current_page = isset($_GET["page"]) ? $_GET["page"] : "1";
+
+$total_page_count = ceil(count($result_array) / PAGE_ITEM_COUNT);
+// print_r($total_page_count);
+$min_display_range = getDisplayItemRange($current_page);
+
+// print_r("最小表示インデックス".$min_display_range."<br>");
+    
+$display_array = array_slice($result_array, $min_display_range, PAGE_ITEM_COUNT);
+// print_r("表示アイテム数".count($display_array));
+echo '<table border="5">
+<tr>';
+foreach($header_array as $header) {
+    echo '<th>'.$header.'</th>';
 }
- 
-// echo '<a href="#title">テスト</a>';
+echo '</tr>';
+foreach($display_array as $array) {
+echo '<tr>';
+    foreach($array as $column) {
+        echo '<td>'.$column.'</td>';
+    }
+    echo '</tr>'; 
+}
+    echo '</table>';
 
-function searchDatas($search_array) {
-    print_r("入った");
+    for ($i = 1; $i <= $total_page_count; $i++) {
+        if ($i == $current_page) {
+            // 表示中ページナンバーはリンクにしない
+            echo  '<a>'.$i.'</a>';
+            continue;
+        }
+        echo  '<a href="?page='.$i.'&property_number='.$input_property_number.'&asset_name='.$input_asset_name.'&auxiliary_submit_name='.$input_auxiliary_submit_name.'">'.$i.'</a>';
+    }
+
+function searchDatas($input_property_number, 
+                    $input_asset_name, 
+                    $input_location, 
+                    $input_auxiliary_submit_name, 
+                    $input_acquisition_price_min,
+                    $input_acquisition_price_max,
+                    $input_book_value_of_period_min,
+                    $input_book_value_of_period_max) {
+    // print_r("入った");
 
 $fileData = new SplFileObject('墨田区固定資産台帳_202103.tsv');
 $fileData -> setFlags(SplFileObject::READ_CSV);
@@ -110,62 +130,58 @@ foreach($fileData as $data) {
 // カラム定義名配列
 $header_array = $imploded_array[0];
 // データ本体の配列
-$data_array = getDataArray($imploded_array, $header_array);
+$data_array = getDataArray($imploded_array);
 
+$result_array = Array();
 // 入力値とデータを比較（絞り込み）
 foreach ($data_array as $column) {
-    $match_flag = false;
+    $match_flag = true;
         
-    foreach(SERCH_TERMS_ARRAY as $team) {
-        if($column[$team] != $search_array[$team] && $search_array[$team] != "") {
-            $match_flag = false;
-            break;
-        }
-        $match_flag = true;
+    if ($input_property_number != "" && $input_property_number != $column[PROPERTY_NUMBER]) {
+        $match_flag = false;
     }
+
+    if ($input_asset_name != "" && $input_asset_name != $column[ASSET_NAME]) {
+        $match_flag = false;
+    }
+
+    if ($input_location != "" && $input_location != $column[LOCATION]) {
+        $match_flag = false;
+    }
+
+    if ($input_auxiliary_submit_name != "" && $input_auxiliary_submit_name != $column[AUXILIARY_SUBJECT_NAME]) {
+        $match_flag = false;
+    }
+  
     if($match_flag) {
          $result_array[] = $column;
     }
 }
 
-$total_page_count = ceil(count($result_array) / PAGE_ITEM_COUNT);
-
-
-
-
-
-    
-    echo '<table border="5">
-    <tr>';
-    foreach($header_array as $header) {
-        echo '<th>'.$header.'</th>';
-    }
-    echo '</tr>';
-    foreach($result_array as $result) {
-        echo '<tr>';
-        foreach($header_array as $index) {
-            echo '<td>'.$result[$index].'</td>';
-        }
-        echo '</tr>'; 
-    }
-    echo '</table>';
+    return [$header_array, $result_array];
 }
 
-function getDataArray($fileData, $header_array) {
+function getDataArray($fileData) {
     // データ名定義の行を削除
     unset($fileData[0]);
     $fileData = array_values($fileData);
     
     // $dataは1行分のデータが入った配列
     foreach($fileData as $record_index => $data) {
-        if (count($data) != 0) {
-            foreach ($header_array as $header_index => $header) {
+        // if (count($data) != 0) {
+            foreach ($data as $data_index => $column) {
             // [資産名称]:墨田区　の形にする（連想配列）
-            $data_array[$record_index][$header] = $data[$header_index];
+            $data_array[$record_index][$data_index] = $column;
             }
-        }
+        // }
     }
     return $data_array;
+}
+
+function getDisplayItemRange($current_page) {
+    $min_display_range = ($current_page * PAGE_ITEM_COUNT) - PAGE_ITEM_COUNT + 1;
+
+    return $min_display_range;
 }
 ?>
 
